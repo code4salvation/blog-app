12495fef4bcd350e13ac3bee65d90e2ba35dabb4
Mehul Saxena <mehul.saxena@nagarro.com>
2024-08-09 19:23:21 +0530

    Fix build issue

--- src/app/(apiroutes)/blog/view/[id]/route.ts
+++ src/app/(apiroutes)/blog/view/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import blogs from "../../../_data/blogs.json";
 
-export async function GET({ params }) {
+export async function GET(_request: Request, { params }) {
     const id = params.id;
     const blogData = blogs.posts.filter((item) => item.id === Number(id));
     return NextResponse.json({ blog: blogData[0] });
