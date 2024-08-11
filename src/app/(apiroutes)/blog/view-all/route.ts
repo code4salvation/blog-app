d12bf76054fd2417d8d6d6a556672506213a9d06
Mehul Saxena <mehul.saxena@nagarro.com>
2024-08-12 00:28:08 +0530

    Update route.ts

--- src/app/(apiroutes)/blog/view-all/route.ts
+++ src/app/(apiroutes)/blog/view-all/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { Pool } from 'pg';
 
 const pool = new Pool({ connectionString: process.env.POSTGRES_URL });
+
+export const dynamic = 'force-dynamic';
 export async function GET() {
     try {
         const client = await pool.connect();
