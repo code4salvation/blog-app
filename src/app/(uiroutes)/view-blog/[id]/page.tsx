00482a1bc62751b0c5e7f803c0aa601fa689f2d9
Mehul Saxena <mehul.saxena@nagarro.com>
2024-08-11 23:16:24 +0530

    Correct page view

--- src/app/(uiroutes)/view-blog/[id]/page.tsx
+++ src/app/(uiroutes)/view-blog/[id]/page.tsx
@@ -1,14 +1,13 @@
 import Box from "@mui/material/Box";
 import styles from "../../page.module.css";
 import ViewBlog from "@/components/blogs/ViewBlog";
-import { BASE_LOCAL, BASE_URL, VIEW_BLOG } from "@/constants/urls";
 import { getBlog } from "@/utils/common";
 
 export default async function Page({params}) {
   const blogData = await getBlog(params?.id);
   return (
     <Box className={styles.main}>
-      <ViewBlog data={blogData}></ViewBlog>
+      <ViewBlog data={blogData?.id ? blogData : blogData?.blog}></ViewBlog>
     </Box>
   );
 }
